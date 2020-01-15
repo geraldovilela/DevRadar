771e2deda951cf3e3695ad815c2d18675d30f63a
Geraldo O. Viela <53988477+geraldovilela@users.noreply.github.com>
2020-01-14 22:15:57 -0400

    Backend feito.

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://sysadmin:Triceratop19@cluster0-hofnt.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+
+//Query params: request.query(filtros, ordenação, paginação)
+//Route params: request.params(identificar um recurso na alteraçao ou remoção)
+//body: request.body (Dados para criação ou alteração de um registro)
+
+app.use(routes)
+
+
+
+
+app.listen(5561); 
